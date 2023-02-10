0297d7158355d9dd1b6c1e9f410cec9a5f2b1d93
Gaurav Belani <gbelani@DESKTOP-AFET4VT.localdomain>
2023-02-10 14:41:00 -0600

    Q1 Part A completed

--- SemaphoreCyclicBarrier.java
+++ SemaphoreCyclicBarrier.java
@@ -1,10 +1,27 @@
+import java.util.concurrent.Semaphore;
+
+
 public class SemaphoreCyclicBarrier implements CyclicBarrier {
 
     private int parties;
+    private int waitCount;
+    private int doneCount;
+    private boolean barrEnable;
+    private Semaphore countLock;
+    private Semaphore wait;
+    private Semaphore cycle;
     // TODO Add other useful variables
 
-    public SemaphoreCyclicBarrier(int parties) {
+    public SemaphoreCyclicBarrier(int parties){
         this.parties = parties;
+        this.waitCount = 0;
+        this.doneCount = 0;
+        this.barrEnable = true;
+        this.countLock = new Semaphore(1);
+        this.wait = new Semaphore(parties);
+        this.wait.drainPermits();
+        this.cycle = new Semaphore(parties);
+
         // TODO Add any other initialization statements
     }
 
@@ -21,7 +38,37 @@ public class SemaphoreCyclicBarrier implements CyclicBarrier {
      */
     public int await() throws InterruptedException {
         // TODO Implement this function
-        return -1;
+        if(!barrEnable){
+            return 0;
+        }
+        else{
+            cycle.acquire();
+            countLock.acquire();
+            int thisInd = waitCount;
+            waitCount++;
+            countLock.release();
+
+            if(waitCount >= parties){
+                wait.release(parties);
+            }
+
+            wait.acquire();
+            countLock.acquire();
+            doneCount++;
+            wait.release();
+
+            if(doneCount >= parties){
+                doneCount = 0;
+                waitCount = 0;
+                wait.drainPermits();
+                cycle.release(parties);
+            }
+            countLock.release();
+            return thisInd;
+
+        }
+
+
     }
 
     /*
@@ -32,6 +79,19 @@ public class SemaphoreCyclicBarrier implements CyclicBarrier {
      */
     public void activate() throws InterruptedException {
         // TODO Implement this function
+        if(barrEnable){
+            return;
+        }
+        else{
+            barrEnable = true;
+            countLock.acquire();
+            waitCount = 0;
+            doneCount = 0;
+            countLock.release();
+            wait.release(parties - wait.availablePermits());
+            wait.drainPermits();
+            return;
+        }
     }
 
     /*
@@ -40,5 +100,14 @@ public class SemaphoreCyclicBarrier implements CyclicBarrier {
      */
     public void deactivate() throws InterruptedException {
         // TODO Implement this function
+        barrEnable = false;
+        wait.release(waitCount);
+        countLock.acquire();
+        waitCount = 0;
+        doneCount = 0;
+        countLock.release();
+        cycle.release(parties - cycle.availablePermits());
+        return;
+
     }
 }
